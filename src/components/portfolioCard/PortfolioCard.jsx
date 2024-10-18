0914a2d273a9324ef8b619aceb52e867de886968
shreeraj007 <shreerajmane007@gmail.com>
2024-10-18 13:29:14 +0530

    [update]:json file + assets

--- src/components/portfolioCard/PortfolioCard.jsx
+++ src/components/portfolioCard/PortfolioCard.jsx
@@ -8,23 +8,32 @@ const PortfolioCard = ({ user }) => {
       <div className="portfolio-web">
         <img src={user["portfolio-hero"]} alt="" />
       </div>
-      <div className="user-dp">
+      {/* <div className="user-dp">
         <img
           className="userImg"
           src={user["profile-photo"]}
           alt={user.username}
         />
-      </div>
+      </div> */}
       <div className="portfolio-content">
-        <h3>{user.username}</h3>
-        <p>{user.title}</p>
-        <a href={user.portfoliolink} target="_blank" rel="noreferrer">
-          {" "}
-          <button className="card-btn">Portfolio</button>
-        </a>
-        <Link to="/userDetails">
-          <button className="card-btn">Details</button>
-        </Link>
+        <div className="user-dp">
+          <img
+            className="userImg"
+            src={user["profile-photo"]}
+            alt={user.username}
+          />
+        </div>
+        <div>
+          <h3>{user.username}</h3>
+          <p>{user.title}</p>
+          <a href={user.portfoliolink} target="_blank" rel="noreferrer">
+            {" "}
+            <button className="card-btn">Portfolio</button>
+          </a>
+          <Link to="/userDetails">
+            <button className="card-btn">Details</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/components/portfolios/Portfolios.jsx
+++ src/components/portfolios/Portfolios.jsx
@@ -1,48 +1,47 @@
 import React from "react";
 import PortfolioCard from "../portfolioCard/PortfolioCard";
 import "./Portfolios.css";
+import sharvariAlawekar from "../../assets/portfoliohero/sharvari-alawekar-hero.png";
+import gauriPawar from "../../assets/portfoliohero/gauri-pawar-hero.png";
 
 // Import your JSON data
 const jsonData = {
   users: {
     user1: {
-      "portfolio-hero": "https://gauriportfolioo.netlify.app/assets/Yummyy.png",
-      "profile-photo": "https://gauriportfolioo.netlify.app/assets/snap1.jpg",
+      "portfolio-hero": gauriPawar,
+      "profile-photo": "https://shorturl.at/AO25X",
       username: "Gauri Pawar",
       title: "Full Stack Developer",
       portfoliolink: "https://gauriportfolioo.netlify.app/",
       linkedIn: "https://www.linkedin.com/public-profile/settings",
     },
     user2: {
-      "portfolio-hero":
-        "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/66a3fbde52d372acd84dbc8a_Nicolas%20Pellegrino%20-%20Wall%20of%20portfolios-p-1080.png",
-      "profile-photo":
-        "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/66a3fc675edfcd6748a0ee72_1709600949949-p-500.jpeg",
+      "portfolio-hero": sharvariAlawekar,
+      "profile-photo": "https://shorturl.at/bFh30",
       username: "Sharavari Alawekar",
       title: "Full Stack Developer",
-      portfoliolink: "https://theshreeraj.vercel.app/",
-      linkedIn: "https://theshreeraj.vercel.app/",
+      portfoliolink: "https://sharvariportfolio.vercel.app/",
+      linkedIn: "https://www.linkedin.com/in/sharvari-alavekar-88a333270/",
     },
-    user3: {
-      "portfolio-hero":
-        "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/66a2661bd5567f2b8e84ff6f_Anirudh%20Goyal%20-%20Wall%20of%20portfolios-p-1080.png",
-      "profile-photo":
-        "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/668c0348723cdd9cbde90b5a_1718310107172.jpeg",
-      username: "Aayan Inamdar",
-      title: "Full Stack Developer",
-      portfoliolink: "https://theshreeraj.vercel.app/",
-      linkedIn: "https://theshreeraj.vercel.app/",
-    },
-    user4: {
-      "portfolio-hero":
-        "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/668a80d91de9f8ecb34c1541_Screenshot%202024-07-07%20at%205.15.51%20PM-p-1080.png",
-      "profile-photo":
-        "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/668a81590fbf2f23d89760ad_1719396842371-p-500.jpeg",
-      username: "Anirudh Goyal",
-      title: "Full Stack Developer",
-      portfoliolink: "https://theshreeraj.vercel.app/",
-      linkedIn: "https://theshreeraj.vercel.app/",
-    },
+    // user3: {
+    //   "portfolio-hero":
+    //     "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/66a2661bd5567f2b8e84ff6f_Anirudh%20Goyal%20-%20Wall%20of%20portfolios-p-1080.png",
+    //   "profile-photo": "https://shorturl.at/bFh30",
+    //   username: "Aayan Inamdar",
+    //   title: "Full Stack Developer",
+    //   portfoliolink: "https://theshreeraj.vercel.app/",
+    //   linkedIn: "https://theshreeraj.vercel.app/",
+    // },
+    // user4: {
+    //   "portfolio-hero":
+    //     "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/668a80d91de9f8ecb34c1541_Screenshot%202024-07-07%20at%205.15.51%20PM-p-1080.png",
+    //   "profile-photo":
+    //     "https://cdn.prod.website-files.com/65c14454c8e90beca1ee62a8/668a81590fbf2f23d89760ad_1719396842371-p-500.jpeg",
+    //   username: "Anirudh Goyal",
+    //   title: "Full Stack Developer",
+    //   portfoliolink: "https://theshreeraj.vercel.app/",
+    //   linkedIn: "https://theshreeraj.vercel.app/",
+    // },
   },
 };
 
